cd8d2d6470ee81442f2e212d2c3005b7327b3d1c
eskabetxe <boto@boto.pro>
2017-05-31 21:19:00 +0200

    added missing files

---
+++ recommender-data/src/main/scala/pro/boto/recommender/data/tables/event/EventColumn.scala
@@ -0,0 +1,9 @@
+package pro.boto.recommender.data.tables.event
+
+object EventColumn {
+  val sessionId:String = "sessionId"
+  val eventTime:String = "eventTime"
+  val userId:String = "userId"
+  val productId:String = "productId"
+  val action:String = "action"
+}
